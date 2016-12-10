e78b4521c2f5b26bfab7867de44c28cfd4217d0a
tolkofor <tolkofor@gmail.com>
2016-12-10 15:16:25 +0000

    trying to write to LED matrix

--- app/src/main/java/com/example/miniproject/omniwheels/FindDevice.java
+++ app/src/main/java/com/example/miniproject/omniwheels/FindDevice.java
@@ -5,8 +5,6 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,6 +12,7 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,12 +20,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
+import static android.os.SystemClock.sleep;
+
 public class FindDevice extends AppCompatActivity {
+
+    private final String LOG_TAG = FindDevice.class.getSimpleName();
 
     private Set<BluetoothDevice> pairedDevices;
     private BluetoothDevice microbit;
@@ -107,74 +109,42 @@ public class FindDevice extends AppCompatActivity {
         //Connect to device
         BluetoothGatt gatt = microbit.connectGatt(this, false, gattCallback);
 
-        //Instantiate private class
-        //ConnectThread ct = new ConnectThread(microbit, mBluetoothAdapter);
+        String uuid = "E95D7B77-251D-470A-A062-FA1922DFA9A8";
+        UUID service = null;
+        try {
+            service = UUID.fromString(uuid);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        boolean begin = gatt.beginReliableWrite();
+        Log.d(LOG_TAG, "Begin write is: " + begin);
+
+        BluetoothGattCharacteristic characteristic =
+                new BluetoothGattCharacteristic(service, 0b1111100000111110000011111, //LED matrix
+                        BluetoothGattCharacteristic.PERMISSION_WRITE);
+
+        boolean write = gatt.writeCharacteristic(characteristic);
+        Log.d(LOG_TAG, "Do write is: " + write);
+
+        boolean execute =  gatt.executeReliableWrite();
+        Log.d(LOG_TAG, "Execute write is: " + execute);
 
     }  //end onCreate
+
+
 
     private  final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+             Log.d(LOG_TAG, "Connection state is: " + newState);
+        }
 
-            Toast t = Toast.makeText(getApplicationContext(),
-                    "Connection state changed", Toast.LENGTH_SHORT);
-            t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-            t.show();
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            Log.d(LOG_TAG, "onCharacteristicWrite is: " + status);
         }
     };
-
-    /**
-     * Connecting as a client - Inner Class
-     */
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-        private final BluetoothAdapter mBluetoothAdapter;
-
-        public ConnectThread(BluetoothDevice device, BluetoothAdapter bluetoothAdapter) {
-            // Use a temporary object that is later assigned to mmSocket,
-            // because mmSocket is final
-            BluetoothSocket tmp = null;
-            this.mmDevice = device;
-            this.mBluetoothAdapter = bluetoothAdapter;
-
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
-            try {
-                // MY_UUID is the app's UUID string, also used by the server code
-                tmp = device.createRfcommSocketToServiceRecord(
-                        (UUID.fromString("what do I put here?")));
-            } catch (IOException e) { }
-            mmSocket = tmp;
-        }
-
-        public void run() {
-            // Cancel discovery because it will slow down the connection
-            mBluetoothAdapter.cancelDiscovery();
-
-            try {
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                mmSocket.connect();
-            } catch (IOException connectException) {
-                // Unable to connect; close the socket and get out
-                try {
-                    mmSocket.close();
-                } catch (IOException closeException) { }
-                return;
-            }
-
-            // Do work to manage the connection (in a separate thread)
-            //manageConnectedSocket(mmSocket);
-        }
-
-
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
 }
 
 
