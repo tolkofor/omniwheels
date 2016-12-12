16d6aa061baeb49811dec0a5d69cb73d380b560a
tolkofor <tolkofor@gmail.com>
2016-12-12 23:12:48 +0000

    Added more buttons to UI

--- app/src/main/java/com/example/miniproject/omniwheels/MainActivity.java
+++ app/src/main/java/com/example/miniproject/omniwheels/MainActivity.java
@@ -1,33 +1,15 @@
 package com.example.miniproject.omniwheels;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-
 public class MainActivity extends AppCompatActivity {
 
-    private OutputStream outputStream;
-    private InputStream inStream;
-    private final static int REQUEST_ENABLE_BT = 100;
-
-    private BluetoothSocket socket = null;
-
-    private final String mac = "67:08:2D:9E:66:0F";
     private boolean connected = false;
 
     @Override
@@ -43,36 +25,24 @@ public class MainActivity extends AppCompatActivity {
         Button topRight = (Button) findViewById(R.id.topRight);
         Button bottomLeft = (Button) findViewById(R.id.bottomLeft);
         Button bottomRight = (Button) findViewById(R.id.bottomRight);
+        Button stop = (Button) findViewById(R.id.stop);
+        Button spinLeft = (Button) findViewById(R.id.spinLeft);
+        Button spinRight = (Button) findViewById(R.id.spinRight);
 
-        up.setOnTouchListener(new View.OnTouchListener() {
+        up.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+            public void onClick(View view) {
 
-                switch (motionEvent.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Toast t = Toast.makeText(getApplicationContext(), "UP", Toast.LENGTH_SHORT);
-                        t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                        t.show();
-                        return true; // if you want to handle the touch event
-                    case MotionEvent.ACTION_UP:
-                        Toast u = Toast.makeText(getApplicationContext(), "STOP UP", Toast.LENGTH_SHORT);
-                        u.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                        u.show();
-                        return true; // if you want to handle the touch event
-                }
-                return false;
+                Toast t = Toast.makeText(getApplicationContext(), "UP", Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
+                t.show();
             }
         });
-
 
         down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String s = String.valueOf(connected);
-                Toast t = Toast.makeText(getApplicationContext(), "Connected: " + connected, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                t.show();
             }
         });
 
@@ -86,44 +56,65 @@ public class MainActivity extends AppCompatActivity {
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), FindDevice.class);
-                startActivity(intent);
+
             }
         });
 
         topLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast t = Toast.makeText(getApplicationContext(), "TOP LEFT", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                t.show();
+
             }
         });
 
         topRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast t = Toast.makeText(getApplicationContext(), "TOP RIGHT", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                t.show();
+
             }
         });
 
         bottomLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast t = Toast.makeText(getApplicationContext(), "BOTTOM LEFT", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                t.show();
+
             }
         });
 
         bottomRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast t = Toast.makeText(getApplicationContext(), "BOTTOM RIGHT", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.BOTTOM | Gravity.CENTER_VERTICAL, 0, 0);
-                t.show();
+
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!connected){
+                    Intent intent = new Intent(getApplicationContext(), FindDevice.class);
+                    connected = true; //for now will have to restart app if it disconnects
+                    startActivity(intent);
+                }
+                else{
+                    //stop
+                }
+
+            }
+        });
+
+        spinLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        spinRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
@@ -131,26 +122,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        connect();
-    }
-
-    private void connect() {
-
-        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
 
